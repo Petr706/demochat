f983334d25d992b11101393815664c50e0678439
Petr706 <55627318+Petr706@users.noreply.github.com>
2022-05-31 17:30:10 +0300

    V2

--- server.js
+++ server.js
@@ -1,19 +1,53 @@
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 3000 });
+const { MongoClient } = require("mongodb");
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url);
 
-server.on('connection', ws => {
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+const server = app.listen(3333, () => {
+    console.log('Server started!');
+});
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
+
+async function addMsgDb(msg) {
+    await client.connect();
+    const db = client.db('Messages');
+    const collection = db.collection('MessagesList');
+    await collection.insertOne({ text: msg, id: Date.now() })
+    await client.close();
+}
+
+wss.on('connection', ws => {
     ws.on('message', message => {
-        message=message.toString('utf8')
-        if(message === 'exit') {
-            ws.close();
-        } else {
-            server.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        })
-        }
-        
+        message = message.toString('utf8')
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                }
+            });
+        addMsgDb(message)
     })
-    ws.send('Добро пожаловать в чат!');
-})
+});
+
+app.get(/\/messages.*/gi, (req, res) => {
+    (async () => {
+        await client.connect();
+        const reqestedPage = parseInt(req.query.page)
+        const db = await client.db('Messages');
+        const collection = await db.collection('MessagesList');
+        const set = new Set(await collection.find().sort({ id: -1 }).limit(reqestedPage*100).toArray())
+        const el = Array.from(set).slice((reqestedPage-1)*100, reqestedPage*100)
+        res.send(`${JSON.stringify(el)}`);
+    })().catch(e => {
+        console.log(e)
+    });
+
+});
