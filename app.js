f437f0a88909d6634620f470da47145d56420c46
Petr706 <55627318+Petr706@users.noreply.github.com>
2022-05-31 17:33:42 +0300

    V2

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
-const status = document.getElementById('status');
 const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-
-const ws = new WebSocket('ws://localhost:3000')
-
-function setStatus(value) {
-    status.innerHTML = value;
-}
-
-function printMessage(value) {
-    const li = document.createElement('li');
-
-    li.innerHTML = value;
-    messages.appendChild(li);
-}
-
-form.addEventListener('submit', event=> {
+const ws = new WebSocket('ws://localhost:3333')
+let messageListCounter = 1
+document.getElementById('sbmt_btn').addEventListener('click', event => {
     event.preventDefault();
     ws.send(input.value);
-    console.log(input.value)
     input.value = '';
-} )
-
-ws.onopen = () => setStatus('ONLINE');
+    const heightNow = getFullHeight()
+    window.scrollBy({
+        top: heightNow,
+        behavior: 'smooth'
+    })
+});
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    ws.send(input.value);
+    input.value = '';
+    const heightNow = getFullHeight()
+    window.scrollBy({
+        top: heightNow,
+        behavior: 'smooth'
+    })
+})
+function loadRecentMsgs() {
+    (async () => {
+        if (window.pageYOffset <= 300) {
+            let response = await fetch(`http://localhost:3333/messages?page=${messageListCounter}`);
+            if(response.ok) {
+                messageListCounter++;
+                let responseObject = await response.json();
+                for (const el of responseObject) {
+                    const li = document.createElement('li');
+                    li.innerHTML = el.text;
+                    messages.insertBefore(li, document.querySelector("li"));
+                }
+            }
+            
+        }
+    })().catch(e => {
+        console.log(e);
+    });
+}
+setInterval(loadRecentMsgs, 500)
+ws.onopen = () => {
+    (async () => {
+        let test = await fetch('http://localhost:3333/messages?page=1')
+        let abc = await test.json()
+        abc = abc.reverse()
+        for (const el of abc) {
+            printMessage(el.text)
+        }
+        const heightNow = getFullHeight()
+        window.scrollBy({
+            top: heightNow,
+            behavior: 'smooth'
+        })
+    })().catch(e => {
+        console.log(e)
+    });
+}
 ws.onclose = () => setStatus('OFFLINE')
 ws.onmessage = response => printMessage(response.data)
