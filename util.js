858ee2ef21eb9f217d05c1e37289b55d6068b721
Petr706 <55627318+Petr706@users.noreply.github.com>
2022-05-31 18:04:26 +0300

    V2

--- util.js
+++ util.js
@@ -9,3 +9,25 @@ export const printMessage = function(value) {
     li.innerHTML = value;
     messages.appendChild(li);
 };
+
+export const loadRecentMsgs = function() {
+    (async () => {
+        if (window.pageYOffset <= 300) {
+            let response = await fetch(`http://localhost:3333/messages?page=${messageListCounter}`);
+            if(response.ok) {
+                messageListCounter++;
+                let responseObject = await response.json();
+                for (const el of responseObject) {
+                    const li = document.createElement('li');
+                    li.innerHTML = el.text;
+                    messages.insertBefore(li, document.querySelector("li"));
+                }
+            }
+            
+        }
+    })().catch(e => {
+        console.log(e);
+    });
+}
+let messageListCounter = 1;
+const messages = document.getElementById('messages');
