449ce657b8a608c8ea435f03a639e6c67d246358
Petr706 <55627318+Petr706@users.noreply.github.com>
2022-05-31 18:04:54 +0300

    V2

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const messages = document.getElementById('messages');
+import {loadRecentMsgs,getFullHeight,printMessage} from './util.js';
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const ws = new WebSocket('ws://localhost:3333')
-let messageListCounter = 1
+const ws = new WebSocket('ws://localhost:3333');
 document.getElementById('sbmt_btn').addEventListener('click', event => {
     event.preventDefault();
     ws.send(input.value);
@@ -23,32 +22,13 @@ form.addEventListener('submit', event => {
         behavior: 'smooth'
     })
 })
-function loadRecentMsgs() {
-    (async () => {
-        if (window.pageYOffset <= 300) {
-            let response = await fetch(`http://localhost:3333/messages?page=${messageListCounter}`);
-            if(response.ok) {
-                messageListCounter++;
-                let responseObject = await response.json();
-                for (const el of responseObject) {
-                    const li = document.createElement('li');
-                    li.innerHTML = el.text;
-                    messages.insertBefore(li, document.querySelector("li"));
-                }
-            }
-            
-        }
-    })().catch(e => {
-        console.log(e);
-    });
-}
 setInterval(loadRecentMsgs, 500)
 ws.onopen = () => {
     (async () => {
         let response = await fetch('http://localhost:3333/messages?page=1')
         let responseObject = await response.json()
         responseObject = responseObject.reverse()
-        for (const el of abc) {
+        for (const el of responseObject) {
             printMessage(el.text)
         }
         const heightNow = getFullHeight()
